48476f0c6d843b2f0c7c4d50c3b0fc0586ae85ed
Michaelhufana22 <hufanamichael521@gmail.com>
2025-08-16 20:24:53 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,2 @@
+print("Magandang hapon \n BSIT-1C")
+print("The Quick red Fox Jumps Over the lazy Dog")
---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name = input("type your name-->")
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\tHi!\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\t\t" , name , "\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
